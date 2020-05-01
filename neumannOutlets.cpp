1a1e3936bfd674834969c8baf07a5d62bb4ab5ef
Xenia <xenia.zagvozdina@yandex.ru>
2020-05-01 16:10:45 +0300

    different uMax

--- neumannOutlets.cpp
+++ neumannOutlets.cpp
@@ -127,7 +127,7 @@ int main(int argc, char* argv[]) {
     global::directories().setOutputDir("./tmp/");
 
     IncomprFlowParam<T> parameters(
-            (T) 1e-2,  // uMax
+            (T) 1e-3,  // uMax
             (T) 300.,  // Re  0.5  1.5
             100,       // N
             2.,        // lx
